189e2fdaae7910fe9e93a044bc7f7208b0bb18c9
jibinkoshy <jibinkoshy@hotmail.com>
2021-05-01 17:42:21 -0500

    Add DB migrations

--- Startup.cs
+++ Startup.cs
@@ -2,9 +2,11 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using CommanderGQL.Data;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -20,6 +22,9 @@ namespace CommanderGQL
         }
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddDbContext<AppDbContext>(opt => opt.UseSqlServer
+            (Configuration.GetConnectionString("CommandConStr"))
+            );
         }
 
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
